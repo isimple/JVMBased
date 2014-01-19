015c821d37146f1e0a5e7b33e65ae8b2f99a1725
simple <warmyh@gmail.com>
2014-01-20 01:00:51 +0800

    Spring MVC without web.xml using WebApplicationInitializer. TODO: integrate groovy

---
+++ src/main/java/config/AppConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * User: yang
+ * Date: 14-1-20
+ * Time: 上午12:11
+ */
+@Configuration
+public class AppConfig {
+}
---
+++ src/main/java/config/DispatcherConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * User: yang
+ * Date: 14-1-20
+ * Time: 上午12:12
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "controller")
+public class DispatcherConfig {
+}
